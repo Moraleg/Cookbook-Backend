fbb1d5659ecf2a1725a6b8cf241cebd6010fb66b
emilycali <cali_emily@bah.com>
2017-05-22 13:48:24 -0400

    changed the allowed methods in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8000', 'http://hungry-for-more-app.herokuapp.com'
+    origins 'localhost:8000', 'https://hungry-for-more-app.herokuapp.com'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :update, :delete, :options, :head]
-      #methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
 
   end
 end
