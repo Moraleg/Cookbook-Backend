76b46b0274a0180a5b7d8f5f89718d3dd69ec297
emilycali <cali_emily@bah.com>
2017-05-21 20:05:32 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-    resources :recipes
+    resources :recipes #do
+      #collection do
+        #get "/", to: "recipes#index"
+      #end
+    #end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
