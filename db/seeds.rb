b3d644c67e4be620d5b7d3883779d2622bbd97d8
Anthony Simmmons <simmonsar@gmail.com>
2017-05-24 23:32:43 -0400

    final Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,91 +13,108 @@
     )
 end
 
+1.times do
+    User.create(
+    username: 'test',
+    password: 'test'
+    )
+
 Recipe.create(
-  title: 'Shrimp and Grits'
-  ingredients:  Faker::StarWars.quote
-  description:  Faker::Lorem.paragraph
-  directions:   Faker::Lorem.paragraph
-  servings:     Faker::Number.between(1, 5),
-  img:          'http://images.media-allrecipes.com/images/61275.jpg'
+  title:'Shrimp and Grits',
+  ingredients:Faker::StarWars.quote,
+  description:Faker::Lorem.paragraph,
+  directions:Faker::Lorem.paragraph,
+  servings:Faker::Number.between(1, 5),
+  img:'http://images.media-allrecipes.com/images/61275.jpg',
+  user_id: 1
 )
 Recipe.create(
-  title: 'Old Charleston Style Shrimp and Grits'
-  ingredients:  Faker::StarWars.quote
-  description:  Faker::Lorem.paragraph
-  directions:   Faker::Lorem.paragraph
-  servings:     Faker::Number.between(1, 5),
-  img:          'http://images.media-allrecipes.com/images/61275.jpg'
+  title:'Old Charleston Style Shrimp and Grits',
+  ingredients:Faker::StarWars.quote,
+  description:Faker::Lorem.paragraph,
+  directions:Faker::Lorem.paragraph,
+  servings:Faker::Number.between(1, 5),
+  img:'http://images.media-allrecipes.com/images/61275.jpg',
+  user_id: 1
 )
 Recipe.create(
-  title: 'Cinnamon Belgian Waffles'
-  ingredients:  Faker::StarWars.quote
-  description:  Faker::Lorem.paragraph
-  directions:   Faker::Lorem.paragraph
-  servings:     Faker::Number.between(1, 5),
-  img:       'http://images.media-allrecipes.com/userphotos/720x405/3571874.jpg'
+  title:'Cinnamon Belgian Waffles',
+  ingredients:Faker::StarWars.quote,
+  description:Faker::Lorem.paragraph,
+  directions:Faker::Lorem.paragraph,
+  servings:Faker::Number.between(1, 5),
+  img:'http://images.media-allrecipes.com/userphotos/720x405/3571874.jpg',
+  user_id: 1
 )
 Recipe.create(
-  title: 'Shrimp and Grits'
-  ingredients:  Faker::StarWars.quote
-  description:  Faker::Lorem.paragraph
-  directions:   Faker::Lorem.paragraph
-  servings:     Faker::Number.between(1, 5),
-  img:          'http://images.media-allrecipes.com/images/61275.jpg'
+  title:'Shrimp and Grits',
+  ingredients:Faker::StarWars.quote,
+  description:Faker::Lorem.paragraph,
+  directions:Faker::Lorem.paragraph,
+  servings:Faker::Number.between(1, 5),
+  img:          'http://images.media-allrecipes.com/images/61275.jpg',
+  user_id: 1
 )
 Recipe.create(
-  title: 'Jalapeno Steak'
-  ingredients:  Faker::StarWars.quote
-  description:  Faker::Lorem.paragraph
-  directions:   Faker::Lorem.paragraph
-  servings:     Faker::Number.between(1, 5),
-  img:          'http://images.media-allrecipes.com/userphotos/720x405/3889279.jpg'
+  title:'Jalapeno Steak',
+  ingredients:Faker::StarWars.quote,
+  description:Faker::Lorem.paragraph,
+  directions:Faker::Lorem.paragraph,
+  servings:Faker::Number.between(1, 5),
+  img:'http://images.media-allrecipes.com/userphotos/720x405/3889279.jpg',
+  user_id: 1
 )
 Recipe.create(
-  title: 'Blackened Catfish and Spicy Rice'
-  ingredients:  Faker::StarWars.quote
-  description:  Faker::Lorem.paragraph
-  directions:   Faker::Lorem.paragraph
-  servings:     Faker::Number.between(1, 5),
-  img:          'http://images.media-allrecipes.com/userphotos/720x405/866245.jpg'
+  title:'Blackened Catfish and Spicy Rice',
+  ingredients:Faker::StarWars.quote,
+  description:Faker::Lorem.paragraph,
+  directions:Faker::Lorem.paragraph,
+  servings:Faker::Number.between(1, 5),
+  img:'http://images.media-allrecipes.com/userphotos/720x405/866245.jpg',
+  user_id: 1
 )
 Recipe.create(
-  title: 'Pumpkin Dessert'
-  ingredients:  Faker::StarWars.quote
-  description:  Faker::Lorem.paragraph
-  directions:   Faker::Lorem.paragraph
-  servings:     Faker::Number.between(1, 5),
-  img:          'http://images.media-allrecipes.com/userphotos/720x405/1568843.jpg'
+  title:'Pumpkin Dessert',
+  ingredients:Faker::StarWars.quote,
+  description:Faker::Lorem.paragraph,
+  directions:Faker::Lorem.paragraph,
+  servings:Faker::Number.between(1, 5),
+  img:          'http://images.media-allrecipes.com/userphotos/720x405/1568843.jpg',
+   user_id: 2
 )
 Recipe.create(
-  title: 'Seafood Fettuccine'
-  ingredients:  Faker::StarWars.quote
-  description:  Faker::Lorem.paragraph
-  directions:   Faker::Lorem.paragraph
-  servings:     Faker::Number.between(1, 5),
-  img:          'http://images.media-allrecipes.com/userphotos/720x405/4502986.jpg'
+  title:'Seafood Fettuccine',
+  ingredients:Faker::StarWars.quote,
+  description:Faker::Lorem.paragraph,
+  directions:Faker::Lorem.paragraph,
+  servings:Faker::Number.between(1, 5),
+  img:'http://images.media-allrecipes.com/userphotos/720x405/4502986.jpg',
+   user_id: 2
 )
 Recipe.create(
-  title: 'Seafood Creole'
-  ingredients:  Faker::StarWars.quote
-  description:  Faker::Lorem.paragraph
-  directions:   Faker::Lorem.paragraph
-  servings:     Faker::Number.between(1, 5),
-  img:          'http://images.media-allrecipes.com/userphotos/720x405/4460829.jpg'
+  title:'Seafood Creole',
+  ingredients:Faker::StarWars.quote,
+  description:Faker::Lorem.paragraph,
+  directions:Faker::Lorem.paragraph,
+  servings:Faker::Number.between(1, 5),
+  img:'http://images.media-allrecipes.com/userphotos/720x405/4460829.jpg',
+   user_id: 2
 )
 Recipe.create(
-  title: 'Mississippi Mud Cheesecake'
-  ingredients:  Faker::StarWars.quote
-  description:  Faker::Lorem.paragraph
-  directions:   Faker::Lorem.paragraph
-  servings:     Faker::Number.between(1, 5),
-  img:          'http://images.media-allrecipes.com/userphotos/560x315/2333905.jpg'
+  title:'Mississippi Mud Cheesecake',
+  ingredients:Faker::StarWars.quote,
+  description:Faker::Lorem.paragraph,
+  directions:Faker::Lorem.paragraph,
+  servings:Faker::Number.between(1, 5),
+  img:'http://images.media-allrecipes.com/userphotos/560x315/2333905.jpg',
+   user_id: 2
 )
 Recipe.create(
-  title: 'Dessert Pizza'
-  ingredients:  Faker::StarWars.quote
-  description:  Faker::Lorem.paragraph
-  directions:   Faker::Lorem.paragraph
-  servings:     Faker::Number.between(1, 5),
-  img:          'http://images.media-allrecipes.com/userphotos/250x250/618338.jpg'
+  title:'Dessert Pizza',
+  ingredients:Faker::StarWars.quote,
+  description:Faker::Lorem.paragraph,
+  directions:Faker::Lorem.paragraph,
+  servings:Faker::Number.between(1, 5),
+  img:'http://images.media-allrecipes.com/userphotos/250x250/618338.jpg',
+   user_id: 2
 )
